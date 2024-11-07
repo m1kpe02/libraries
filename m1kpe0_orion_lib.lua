605de2551419757f94c9bc93ef1e933dc6100a45
m1kpe02 <yarikasiktwink@inbox.ru>
2024-11-07 22:14:28 +0300

    Update m1kpe0_orion_lib.lua

--- m1kpe0_orion_lib.lua
+++ m1kpe0_orion_lib.lua
@@ -18,7 +18,7 @@ local OrionLib = {
 			Stroke = Color3.fromRGB(31, 0, 31),
 			Divider = Color3.fromRGB(1, 0, 1),
 			Text = Color3.fromRGB(255, 255, 255),
-			TextDark = Color3.fromRGB(102, 0, 102)
+			TextDark = Color3.fromRGB(82, 0, 82)
 		}
 	},
 	SelectedTheme = "Default",
