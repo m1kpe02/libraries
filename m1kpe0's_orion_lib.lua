877c1c35b1b1825c6e40dfa7972a4b3492e991b8
m1kpe02 <yarikasiktwink@inbox.ru>
2024-11-07 20:58:52 +0300

    Update m1kpe0's_orion_lib.lua

--- m1kpe0's_orion_lib.lua
+++ m1kpe0's_orion_lib.lua
@@ -15,7 +15,7 @@ local OrionLib = {
 			Main = Color3.fromRGB(21, 0, 21),
 			Second = Color3.fromRGB(31, 0, 31),
 			Stroke = Color3.fromRGB(51, 0, 51),
-			Divider = Color3.fromRGB(01, 0, 01),
+			Divider = Color3.fromRGB(1, 0, 1),
 			Text = Color3.fromRGB(255, 255, 255),
 			TextDark = Color3.fromRGB(150, 150, 150)
 		}
